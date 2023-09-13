f6a735375bda6823224b8be7e3b9a2904b567386
unknown <oppongcoffie27@gmail.com>
2023-09-13 10:42:19 +0000

    all

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <!-- TOP MOST CONTENT -->
+    <div class="heading">
+        <div>
+            <div id="topname" class="name">equalizer</div> 
+            <div class="uses">We make your music sound  extraordinary.</div>
+        </div>
+        <div class="">
+            <img class="pattern-1" src="./equalizer-landing-page/equalizer-landing-page/starter-code/assets/bg-pattern-1.svg" alt="">
+        </div>
+    </div>
+
+    <div class="text">
+        A system audio equalizer specifically design for Android iOS. Freely tune the way your music sounds with a professional grade parametric EQ & volume miser. Control bass, mids, treble, gain control, reverb, and more!
+    </div>
+    <div class="card">
+
+        <div class="phone">
+        <img class="phone-pic" src="./equalizer-landing-page/equalizer-landing-page/starter-code/assets/illustration-app.png" alt="">
+        </div>
+
+        <div class="icon">
+        <img class="pattern-2" src="./equalizer-landing-page/equalizer-landing-page/starter-code/assets/bg-pattern-2.svg" alt="">
+        </div>
+
+        <div class="card2">
+            <h1>Premium EQ</h1>
+            <div class="">
+                Get expert-level control with a <br> robust equalizer, volume mixer, and <br> spatial audio. Take your listening <br> experience to a whole new level and <br> access all our incredible features!
+            </div>
+            <h1>$4 <span class="month">/month</span></h1>
+            <button class="ios">
+                <img src="./equalizer-landing-page/equalizer-landing-page/starter-code/assets/icon-apple.svg" alt="">    
+                iOS Download
+            </button>
+            <button class="android">
+            <img src="./equalizer-landing-page/equalizer-landing-page/starter-code/assets/icon-android.svg" alt=""> 
+            Android Download</button>
+        </div>
+
+    </div>
+
+
+    <div class="footer">
+
+        <div class="name">equalizer</div>
+
+        <div class="et">All rights reserved © Equalizer 2021<br>
+            Have any problems? Contact us via social media or<br>
+             email us<b>at equalizer@example.com<b></div>
+
+        <div class="equalizer-icons">
+            
+            
+            <img id="iii" src="./images/download.png" alt="">
+            <img id="iii" src="./images/ins3.png" alt="">
+            <img id="iii" src="./images/twi.png" alt="">
+                
+           
+        </div>
+</div>
+
+    
+
+</body>
+</html>
